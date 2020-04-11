cd16d87034d8109b17eac236e1a4a3c1270f27ef
Faizy51 <faizulla49@yahoo.co.uk>
2020-04-11 12:19:25 +0530

    Initial Commit

---
+++ SingAut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingAut
+//
+//  Created by Faizyy on 11/04/20.
+//  Copyright © 2020 faiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
